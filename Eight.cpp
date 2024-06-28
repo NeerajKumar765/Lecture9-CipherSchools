47cbc1bfcb660293e9801eefc6bc9b4a0876d7f4
Neeraj Kumar <neerajkumar42p@gmail.com>
2024-06-28 23:32:17 +0530

    Commit

---
+++ Eight.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for(i = 0; i < 10; i++) {  // Adding condition and increment operation
+        cout << i << " ";
+    }
+    return 0;  // Adding return statement to indicate successful completion
+}
